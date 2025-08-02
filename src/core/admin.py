d38e745449fca180161307a509e337a602260c34
logan lugez <binglei.pro@gmail.com>
2025-08-02 16:12:01 +0200

    create instance customer after sending form

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Customer
 
-admin.site.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'email', 'created_at')
+    search_fields = ('first_name', 'email')
+    list_filter = ('created_at',)
+    readonly_fields = ('created_at',)
+
+admin.site.register(Customer, CustomerAdmin)
--- src/core/urls.py
+++ src/core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("success/<int:customer_id>/", views.success_view, name="success"),
 ]
