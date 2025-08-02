63d6d311660653bead52f5e1bff855fb49fde972
logan lugez <binglei.pro@gmail.com>
2025-08-02 16:34:14 +0200

    ok

--- src/core/models.py
+++ src/core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -7,6 +8,7 @@ class Customer(models.Model):
     last_name = models.CharField(max_length=30)
     phone = models.CharField(max_length=12)
     email = models.EmailField(max_length=254)
+    created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.first_name
